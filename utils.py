b1cdedac6d5511676608a741510c17075099a3c9
Joy-aa <1663607838@qq.com>
2022-11-02 16:02:07 +0800

    init

--- utils.py
+++ utils.py
@@ -1,205 +1,48 @@
-import multiprocessing
-import os
-import random
-
-import cv2 as cv
-import keras.backend as K
 import numpy as np
-import tensorflow as tf
-from tensorflow.python.client import device_lib
-from tqdm import tqdm
+import os
+import cv2
 
-from config import alpha, identity_annot_filename, num_train_samples, lfw_folder
-
-
-def ensure_folder(folder):
-    if not os.path.exists(folder):
-        os.makedirs(folder)
+# 数据读入
+image_types = (".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff")
 
 
-# getting the number of GPUs
-def get_available_gpus():
-    local_device_protos = device_lib.list_local_devices()
-    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+# type(image_types) = tuple
 
+def list_images(basePath, contains=None):
+    # 返回有效的图片路径
+    for (rootDir, dirNames, filenames) in os.walk(basePath):
+        # 循环遍历当前目录中的所有dir
+        for filename in filenames:
+            # 如果contains为None就不会进入后半部分，避免报错
+            if contains is not None and filename.find(contains) == -1:
+                continue
 
-# getting the number of CPUs
-def get_available_cpus():
-    return multiprocessing.cpu_count()
+            # 确定文件中的拓展名
+            ext = filename[filename.rfind("."):].lower()
 
+            # 检查文件是否为图像，若是图像加入生成器中
+            if image_types is None or ext.endswith(image_types):
+                imagePath = os.path.join(rootDir, filename)
+                yield imagePath
 
-def draw_str(dst, target, s):
-    x, y = target
-    cv.putText(dst, s, (x + 1, y + 1), cv.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 0), thickness=2, lineType=cv.LINE_AA)
-    cv.putText(dst, s, (x, y), cv.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), lineType=cv.LINE_AA)
+def load_data(path):
+    # 开始读取数据
+    data = []
+    labels = []
+    imagePaths = list_images("./test_dataset")
 
+    for imagePath in imagePaths:
+        # 读取图像数据
+        image = cv2.imread(imagePath)
+        # 可以降低图像像素点，下采样
+        image = cv2.resize(image, (28, 28))
+        # 转换成灰度图
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        data.append(image)
+        label = imagePath.split(os.path.sep)[-2]
+        labels.append(label)
+    return data, labels
 
-def get_excludes():
-    with open('data/exclude.txt') as file:
-        lines = file.readlines()
-    lines = [line.strip() for line in lines]
-    return lines
-
-
-# Get statistics for train data
-def get_data_stats():
-    with open(identity_annot_filename, 'r') as file:
-        lines = file.readlines()
-
-    ids = set()
-    images = []
-    image2id = {}
-    id2images = {}
-    excludes = get_excludes()
-
-    for line in lines:
-        line = line.strip()
-        if len(line) > 0:
-            tokens = line.split(' ')
-            image_name = tokens[0].strip()
-            if image_name not in excludes and image_name != '202599.jpg':
-                id = tokens[1].strip()
-                ids.add(id)
-                images.append(image_name)
-                image2id[image_name] = id
-                if id in id2images.keys():
-                    id2images[id].append(image_name)
-                else:
-                    id2images[id] = [image_name]
-
-    return list(ids), sorted(images), image2id, id2images
-
-
-def triplet_loss(y_true, y_pred):
-    a_pred = y_pred[:, 0:128]
-    p_pred = y_pred[:, 128:256]
-    n_pred = y_pred[:, 256:384]
-    positive_distance = K.square(tf.norm(a_pred - p_pred, axis=-1))
-    negative_distance = K.square(tf.norm(a_pred - n_pred, axis=-1))
-    loss = K.mean(K.maximum(0.0, positive_distance - negative_distance + alpha))
-    return loss
-
-
-def get_random_triplets():
-    # Random selection of validation set samples
-    ids, images, image2id, id2images = get_data_stats()
-
-    images = images[:num_train_samples]
-    num_random_triplets = num_train_samples
-
-    data_set = []
-
-    for i in tqdm(range(num_train_samples)):
-        # choose a_image
-        while True:
-            a_image = random.choice(images)
-            a_id = image2id[a_image]
-            if len(id2images[a_id]) >= 2: break
-
-        # choose p_image
-        while True:
-            p_image = random.choice(id2images[a_id])
-            if p_image != a_image: break
-
-        # choose n_image
-        while True:
-            n_image = random.choice(images)
-            n_id = image2id[n_image]
-            if n_id != a_id: break
-
-        data_set.append({'a': a_image, 'p': p_image, 'n': n_image})
-
-    return data_set
-
-
-def get_train_images():
-    _, images, _, _ = get_data_stats()
-    return images
-
-
-def get_lfw_images():
-    with open('data/people.txt', 'r') as file:
-        lines = file.readlines()
-
-    names = []
-
-    for i in (range(2, len(lines))):
-        line = lines[i].strip()
-        tokens = line.split()
-        if len(tokens) > 1:
-            person_name = tokens[0]
-            count = int(tokens[1])
-            for j in range(1, count + 1):
-                name = '{0}/{0}_{1}.jpg'.format(person_name, str(j).zfill(4))
-                filename = os.path.join(lfw_folder, name)
-                if os.path.isfile(filename):
-                    names.append(name)
-                else:
-                    raise Exception('File Not Found: {}'.format(filename))
-
-    return names
-
-
-def get_lfw_pairs():
-    with open('data/pairs.txt', 'r') as file:
-        lines = file.readlines()
-
-    pairs = []
-
-    for i in (range(1, len(lines))):
-        line = lines[i].strip()
-        tokens = line.split()
-        if len(tokens) == 3:
-            person_name = tokens[0]
-            id1 = int(tokens[1])
-            id2 = int(tokens[2])
-            image_name_1 = '{0}/{0}_{1}.jpg'.format(person_name, str(id1).zfill(4))
-            image_name_2 = '{0}/{0}_{1}.jpg'.format(person_name, str(id2).zfill(4))
-            pairs.append({'image_name_1': image_name_1, 'image_name_2': image_name_2, 'same_person': True})
-        elif len(tokens) == 4:
-            person_name_1 = tokens[0]
-            id1 = int(tokens[1])
-            person_name_2 = tokens[2]
-            id2 = int(tokens[3])
-            image_name_1 = '{0}/{0}_{1}.jpg'.format(person_name_1, str(id1).zfill(4))
-            image_name_2 = '{0}/{0}_{1}.jpg'.format(person_name_2, str(id2).zfill(4))
-            pairs.append({'image_name_1': image_name_1, 'image_name_2': image_name_2, 'same_person': False})
-
-    return pairs
-
-
-def get_smallest_loss():
-    import re
-    pattern = 'model.(?P<epoch>\d+)-(?P<val_loss>[0-9]*\.?[0-9]*).hdf5'
-    p = re.compile(pattern)
-    losses = [float(p.match(f).groups()[1]) for f in os.listdir('models/') if p.match(f)]
-    if len(losses) == 0:
-        import sys
-        return sys.float_info.max
-    else:
-        return np.min(losses)
-
-
-def get_latest_model():
-    import glob
-    import os
-    files = glob.glob('models/*.hdf5')
-    files.sort(key=os.path.getmtime)
-    if len(files) > 0:
-        return files[-1]
-    else:
-        return None
-
-
-def get_best_model():
-    import re
-    pattern = 'model.(?P<epoch>\d+)-(?P<val_loss>[0-9]*\.?[0-9]*).hdf5'
-    p = re.compile(pattern)
-    files = [f for f in os.listdir('models/') if p.match(f)]
-    filename = None
-    if len(files) > 0:
-        losses = [float(p.match(f).groups()[1]) for f in files]
-        best_index = int(np.argmin(losses))
-        filename = os.path.join('models', files[best_index])
-        print('loading best model: {}'.format(filename))
-    return filename
+# 对图像进行完scale操作之后可以看到与原图像保持了一致
+data = np.array(data, dtype='float') / 255;
+labels = np.array(labels)
